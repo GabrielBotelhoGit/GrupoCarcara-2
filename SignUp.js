784710d7e65b8a20bca2f2f41d469f0c55e54d15
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-01-31 11:36:39 -0300

    branch para o signup

--- SignUp.js
+++ SignUp.js
@@ -3,4 +3,33 @@ const baseURL = getBaseUrl();
 $(document).ready(function(){
     let nav = montarNavBar();
     document.getElementById('navbanco').innerHTML = nav;
+
+    document.getElementById('submit_new_register').addEventListener('click', () => {
+        let userName = document.getElementById('username').value,
+            userEmail = document.getElementById('email').value,
+            userPassword = document.getElementById('password').value,
+            userRepassword = document.getElementById('re_password').value,
+            userCpf = document.getElementById('cpf').value.replace(/[^\d]/g, "");
+        
+        if (userPassword === userRepassword) {
+            axios.post(`${baseURL}usuarios`, {
+                cpf: userCpf,
+                login: userEmail,
+                nome: userName,
+                senha: userPassword
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => {
+                
+                if (res.status === 200) {
+                    window.location.replace('#/login');
+                }
+            })
+        } else {
+            console.log('Confira a sua senha!');
+        }
+    });
 });
